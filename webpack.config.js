f728fa569293dc27b408e629d7f79c1b3c0d80b4
braqt <brian.quinteros216@gmail.com>
2023-07-28 16:50:28 -0400

    new pacakage installed and added to webpack.config.js: copy-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 const path = require("path");
+const Dotenv = require("dotenv-webpack");
+const CopyPlugin = require("copy-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const Dotenv = require("dotenv-webpack");
 
 module.exports = {
   entry: "./src/index.tsx",
@@ -45,6 +46,14 @@ module.exports = {
       chunkFilename: "[name].[chunkhash].chunk.css",
     }),
     new Dotenv(),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "public"),
+          to: "./",
+        },
+      ],
+    }),
   ],
   devServer: {
     open: true,

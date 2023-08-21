f3aaee663350d4d57ee1301255d59aace6ab0953
braqt <brian.quinteros216@gmail.com>
2023-08-20 21:55:47 -0400

    Refactor /admin/products page

---
+++ src/components/panels/Admin/Products/index.tsx
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from "react";
+
+import globalStyles from "../../../../index.module.css";
+import styles from "../index.module.css";
+
+import { useAuthentication } from "../../../../context/auth";
+import { Product } from "../../../../services/interfaces";
+import { getAdminProducts } from "../../../../services/adminService";
+import StyledInput from "../../../Input/StyledInput";
+import SpinnerLoader from "../../../loaders/spinnerLoader";
+import ProductsTable from "../../../ProductsTable";
+
+interface Props {
+  onClickCreateProduct: () => void;
+}
+
+const ProductsPanel = ({ onClickCreateProduct }: Props) => {
+  const { user } = useAuthentication();
+  const [products, setProducts] = useState<Product[]>([]);
+  const [name, setName] = useState("");
+  const [loadingProducts, setLoadingProducts] = useState(true);
+
+  const fetchAndSetProducts = async () => {
+    if (user) {
+      setLoadingProducts(true);
+      const firebaseAuthToken = await user.getIdToken();
+      const response = await getAdminProducts(1, 40, firebaseAuthToken, {
+        name,
+      });
+      setProducts(response.products);
+      setLoadingProducts(false);
+    }
+  };
+
+  const onChangeName = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+  };
+
+  useEffect(() => {
+    fetchAndSetProducts();
+  }, []);
+
+  return (
+    <div className={globalStyles.pageFrame2}>
+      <div className={styles.title}>Products</div>
+      <div className={styles.operationFrame}>
+        <div className={styles.filters}>
+          <StyledInput value={name} onChange={onChangeName} />
+          <button onClick={fetchAndSetProducts}>search</button>
+        </div>
+        <button onClick={onClickCreateProduct}>+ add product</button>
+      </div>
+      <div style={{ marginTop: "18px", height: "300px", position: "relative" }}>
+        {loadingProducts && <SpinnerLoader />}
+        {!loadingProducts && <ProductsTable products={products} />}
+      </div>
+    </div>
+  );
+};
+
+export default ProductsPanel;
--- src/pages/admin/products/index.tsx
+++ src/pages/admin/products/index.tsx
@@ -1,15 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
-import globalStyles from "../../../index.module.css";
-import styles from "./index.module.css";
-
-import { Product } from "../../../services/interfaces";
-import { getAdminProducts } from "../../../services/adminService";
-import { useAuthentication } from "../../../context/auth";
-import StyledInput from "../../../components/Input/StyledInput";
-import ProductsTable from "../../../components/ProductsTable";
-import SpinnerLoader from "../../../components/loaders/spinnerLoader";
 import AddProductPanel from "../../../components/panels/Admin/CreateProduct";
+import ProductsPanel from "../../../components/panels/Admin/Products";
 
 enum ProductPagePanel {
   PRODUCTS = "PRODUCTS",
@@ -17,29 +9,9 @@ enum ProductPagePanel {
 }
 
 const AdminProductsPage = () => {
-  const { user } = useAuthentication();
-  const [products, setProducts] = useState<Product[]>([]);
-  const [name, setName] = useState("");
-  const [loadingProducts, setLoadingProducts] = useState(true);
   const [panel, setPanel] = useState<ProductPagePanel>(
-    ProductPagePanel.CREATE_PRODUCT
+    ProductPagePanel.PRODUCTS
   );
-
-  const fetchAndSetProducts = async () => {
-    if (user) {
-      setLoadingProducts(true);
-      const firebaseAuthToken = await user.getIdToken();
-      const response = await getAdminProducts(1, 40, firebaseAuthToken, {
-        name,
-      });
-      setProducts(response.products);
-      setLoadingProducts(false);
-    }
-  };
-
-  const onChangeName = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setName(e.target.value);
-  };
 
   const onCreateProduct = () => {
     setPanel(ProductPagePanel.PRODUCTS);
@@ -53,10 +25,6 @@ const AdminProductsPage = () => {
     setPanel(ProductPagePanel.PRODUCTS);
   };
 
-  useEffect(() => {
-    fetchAndSetProducts();
-  }, []);
-
   return (
     <>
       {panel == ProductPagePanel.CREATE_PRODUCT && (
@@ -66,22 +34,7 @@ const AdminProductsPage = () => {
         />
       )}
       {panel == ProductPagePanel.PRODUCTS && (
-        <div className={globalStyles.pageFrame2}>
-          <div className={styles.title}>Products</div>
-          <div className={styles.operationFrame}>
-            <div className={styles.filters}>
-              <StyledInput value={name} onChange={onChangeName} />
-              <button onClick={fetchAndSetProducts}>search</button>
-            </div>
-            <button onClick={onClickCreateProduct}>+ add product</button>
-          </div>
-          <div
-            style={{ marginTop: "18px", height: "300px", position: "relative" }}
-          >
-            {loadingProducts && <SpinnerLoader />}
-            {!loadingProducts && <ProductsTable products={products} />}
-          </div>
-        </div>
+        <ProductsPanel onClickCreateProduct={onClickCreateProduct} />
       )}
     </>
   );

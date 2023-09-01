5b3067a438e7f0c63d2c937fa39767143f4c76ff
braqt <brian.quinteros216@gmail.com>
2023-09-01 19:00:55 -0400

    now the buttons from layoutWithSideNavbarAdmin component sends the user to its respective page

--- src/components/layouts/layoutWithSideNavbarAdmin/index.tsx
+++ src/components/layouts/layoutWithSideNavbarAdmin/index.tsx
@@ -3,21 +3,44 @@ import React from "react";
 import styles from "./index.module.css";
 
 import { MdDescription, MdInventory, MdPerson } from "react-icons/md";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
+
+import {
+  ADMIN_ORDERS,
+  ADMIN_PRODUCTS,
+  ADMIN_USERS,
+} from "../../../navigation/pagePaths";
 
 const LayoutWithSideNavBarAdmin = () => {
+  const navigate = useNavigate();
+
   return (
     <div className={styles.frame}>
       <div className={styles.sideNavBar}>
-        <div className={styles.sideNavBarOption}>
+        <div
+          className={styles.sideNavBarOption}
+          onClick={() => {
+            navigate(ADMIN_PRODUCTS);
+          }}
+        >
           <MdInventory />
           <div>Products</div>
         </div>
-        <div className={styles.sideNavBarOption}>
+        <div
+          className={styles.sideNavBarOption}
+          onClick={() => {
+            navigate(ADMIN_USERS);
+          }}
+        >
           <MdPerson />
-          <div>User</div>
+          <div>Users</div>
         </div>
-        <div className={styles.sideNavBarOption}>
+        <div
+          className={styles.sideNavBarOption}
+          onClick={() => {
+            navigate(ADMIN_ORDERS);
+          }}
+        >
           <MdDescription />
           <div>Orders</div>
         </div>
